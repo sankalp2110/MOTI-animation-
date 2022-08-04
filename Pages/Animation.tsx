402f258ff5f8e3e97b45ca24d2d901dc3ee7ae49
Sankalp Nihal Pandey <sankalpnihalpandey@Sankalp-nihal-Pandey-MacBook-Air.local>
2022-08-04 16:29:11 +0530

    moti animation

--- Pages/Animation.tsx
+++ Pages/Animation.tsx
@@ -1,192 +1,7 @@
-// import React from "react";
-// import { Text, Box, Icon, Center, VStack, Button } from "native-base";
-// import { MaterialIcons } from "@expo/vector-icons";
-// import { MotiView } from "moti";
-// import { Easing } from "react-native-reanimated";
-// const LoadingIndicator = ({ size }) => {
-//   return (
-//     <MotiView
-//       from={{
-//         width: size,
-//         height: size,
-//         borderRadius: size / 2,
-//         borderWidth: 0.3,
-//         shadowOpacity: 0.2,
-//       }}
-//       animate={{
-//         width: size + 20,
-//         height: size + 20,
-//         borderRadius: size / 2,
-//         borderWidth: size / 10,
-//         shadowOpacity: 1,
-//       }}
-//       transition={{
-//         type: "timing",
-//         duration: 2000,
-//         loop: true,
-//       }}
-//       style={{
-//         width: size,
-//         height: size,
-//         borderRadius: size / 2,
-//         borderWidth: size / 10,
-//         borderColor: "#b91c1c",
-//         shadowColor: "#b91c1c",
-//         shadowOffset: { width: 0, height: 0 },
-//         shadowOpacity: 1,
-//         shadowRadius: 10,
-//       }}
-//     />
-//   );
-// };
-// export default function Home() {
-//   return (
-//     <>
-//       <Box safeAreaTop />
-//       <VStack>
-//         <MotiView></MotiView>
-//       </VStack>
-//       <Text>rgnirg</Text>
-//     </>
-//   );
-// }
-
-// const TextStyle = {
-//   color: "coolGray.50",
-//   fontWeight: "bold",
-//   textAlign: "center",
-//   fontSize: "3xl",
-// };
-// export default function App() {
-//   return (
-//     <>
-//       <Box safeAreaTop />
-//       <Text textAlign='center' fontWeight='semibold' fontSize='3xl'>
-//         Animation
-//       </Text>
-//       <VStack px={4} justifyContent='center'>
-//         <MotiView
-//           from={{
-//             translateY: -200,
-//           }}
-//           animate={{
-//             translateY: 50,
-//           }}
-//           transition={{
-//             loop: true,
-//             type: "timing",
-//             duration: 1500,
-//             delay: 100,
-//           }}
-//         >
-//           <Button
-//             bg='amber.800'
-//             onPress={() => {
-//               alert("test");
-//             }}
-//           >
-//             Primary
-//           </Button>
-//         </MotiView>
-// </VStack>
-{
-  /* <Box
-        flex={1}
-        alignItems="center"
-        justifyContent="center"
-        bg="coolGray.900"
-      > */
-}
-{
-  /* <Box alignItems="center" justifyContent="center">
-          <MotiView
-            from={{ translateY: 300 }}
-            animate={{
-              translateY: 50,
-            }}
-            transition={{
-              type: "spring",
-              loop: true,
-            }}
-            style={{
-              height: 250,
-              width: 250,
-              backgroundColor: "#991b1b",
-              borderRadius: 20,
-            }}
-          >
-            <VStack
-              space={2}
-              justifyContent="center"
-              alignItems="center"
-              mt={5}
-            >
-              <Text {...TextStyle}>MOTI</Text>
-              <Text {...TextStyle}>ANIMATION</Text>
-              <Text {...TextStyle}>WITH</Text>
-              <Text {...TextStyle}>NATIVEBASE</Text>
-            </VStack>
-          </MotiView>
-        </Box> */
-}
-{
-  /* <LoadingIndicator size={100} /> */
-}
-{
-  /* <Box
-          width="100"
-          height="100"
-          borderRadius="100"
-          bg="red.700"
-          alignItems="center"
-          justifyContent="center"
-        >
-          {[...Array(3).keys()].map((index) => {
-            return (
-              <MotiView
-                key={index}
-                from={{ opacity: 0.6, scale: 1 }}
-                animate={{ opacity: 0, scale: 4 }}
-                transition={{
-                  type: "timing",
-                  duration: 2000,
-                  easing: Easing.out(Easing.ease),
-                  delay: index * 400,
-                  loop: true,
-                  repeatReverse: false,
-                }}
-                style={{
-                  position: "absolute",
-                  width: 100,
-                  height: 100,
-                  borderRadius: 100,
-                  backgroundColor: "#b91c1c",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              />
-            );
-          })}
-          <Icon
-            as={MaterialIcons}
-            name="phone-callback"
-            color="white"
-            size={16}
-          />
-        </Box> */
-}
-{
-  /* </Box> */
-}
-//     </>
-//   );
-// }
-
-import React, { useEffect, useState, useReducer } from "react";
-import { Text, Box, Icon, VStack, Button, Pressable } from "native-base";
+import React, { useEffect, useState } from "react";
+import { Text, Box, Icon, VStack, Button } from "native-base";
 import { Feather } from "@expo/vector-icons";
 import { MotiView } from "moti";
-import { Transition } from "react-native-reanimated";
 
 function ButtonAnimation({
   size,
@@ -288,59 +103,17 @@ function CheckCircle({
 }
 
 export default function Animation() {
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "INCREMENT":
-        return {
-          count: state.count + 1,
-          showText: state.showText,
-        };
-      case "CHANGETEXT":
-        return {
-          count: state.count,
-          showText: !state.showText,
-        };
-      default:
-        return state;
+  const [width, setWidth] = useState(400);
+  const [buttonscale, setButtonscale] = useState(1);
+  const [success, setSuccess] = useState(0);
+  useEffect(() => {
+    if (buttonscale == 0) {
+      setSuccess(1);
     }
-  };
-  const [state, dispatch] = useReducer(reducer, {
-    count: 0,
-    showText: true,
-  });
-
-  // const [text, setShowText] = useState(true);
-  // const [count, setCount] = useState(0);
-
-  // <Button
-  //   colorScheme='primary'
-  //   onPress={() => {
-  //     console.log("hello");
-  //   }}
-  // >
-  //   Primary
-  // </Button>;
-
-  // const [width, setWidth] = useState(400);
-  // const [buttonscale, setButtonscale] = useState(1);
-  // const [success, setSuccess] = useState(0);
-  // useEffect(() => {
-  //   if (buttonscale == 0) {
-  //     setSuccess(1);
-  //   }
-  // }, [buttonscale]);
+  }, [buttonscale]);
   return (
     <>
       <Box safeAreaTop />
-      <Text
-        textAlign='center'
-        justifyContent='center'
-        color='black'
-        fontSize='lg'
-        fontWeight='semibold'
-      >
-        hooks practice
-      </Text>
 
       <VStack
         bg='coolGray.800'
@@ -350,27 +123,7 @@ export default function Animation() {
         safeAreaBottom
         alignItems='center'
       >
-        <Text color='white' textAlign='center' fontWeight='bold'>
-          {state.count}
-        </Text>
-        <Button
-          width='30%'
-          onPress={() => {
-            dispatch({ type: "INCREMENT" });
-            dispatch({ type: "CHANGETEXT" });
-            // setCount(count + 1);
-            // setShowText(text);
-          }}
-        >
-          increment
-        </Button>
-        {state.showText && (
-          <Text color='white' fontWeight='bold' fontSize='2xl'>
-            toggle text
-          </Text>
-        )}
-
-        {/* <CheckCircle
+        <CheckCircle
           size={width}
           onPress={(width) => setWidth(width)}
           success={success}
@@ -379,7 +132,7 @@ export default function Animation() {
           size={width}
           buttonscale={buttonscale}
           onPress={(buttonwidth) => setButtonscale(buttonwidth)}
-        /> */}
+        />
       </VStack>
     </>
   );
