ae0674ce49461c047c4508018b0c6d02e56f82f0
Sankalp Nihal Pandey <sankalpnihalpandey@SankalpandeyAir.bbrouter>
2022-08-14 11:16:32 +0530

    animation

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import React from "react";
-import { NativeBaseProvider, extendTheme } from "native-base";
-import Animation from "./Pages/Animation";
+// import React from "react";
+// import { NativeBaseProvider, extendTheme } from "native-base";
+// import Animation from "./Pages/Animation";
 
+// export default function App() {
+//   return (
+//     <NativeBaseProvider>
+//       <Animation />
+//     </NativeBaseProvider>
+//   );
+// }
+
+import React from "react";
+import { NativeBaseProvider, extendTheme, theme } from "native-base";
+import ExitBeforeEnter from "./Pages/ExitBeforeEnter";
+
+const customTheme = extendTheme({
+  colors: {
+    primary: { ...theme.colors.violet },
+    secondary: "#eab308",
+  },
+  sizes: {
+    1: 4,
+    2: 8,
+    3: 12,
+    4: 16,
+    5: 20,
+  },
+});
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Animation />
+    <NativeBaseProvider theme={customTheme}>
+      <ExitBeforeEnter />
+      {/* <ButtonAnimation size={40} buttonscale={1} onPress={() => {}} /> */}
     </NativeBaseProvider>
   );
 }
---
+++ Pages/ExitBeforeEnter.tsx
@@ -0,0 +1,93 @@
+import { AnimatePresence } from "framer-motion";
+import React, { useReducer } from "react";
+import { StyleSheet } from "react-native";
+import { MotiView } from "moti";
+import { Pressable, Text, theme, Button } from "native-base";
+
+function AnimatedButton({
+  bg,
+  text,
+  visible,
+}: {
+  bg: string;
+  text: string;
+  visible: boolean;
+}) {
+  return (
+    <MotiView
+      from={{
+        opacity: 0,
+        scale: 0.5,
+      }}
+      animate={{
+        opacity: 1,
+        scale: 1,
+      }}
+      transition={{
+        type: "timing",
+        duration: 1000,
+      }}
+      exit={{
+        opacity: 0,
+        scale: 0.9,
+      }}
+      style={[
+        styles.shape,
+        {
+          backgroundColor: bg,
+          // height: visible ? theme.sizes[10] : theme.sizes[16],
+        },
+      ]}
+    >
+      <Text
+        textAlign='center'
+        fontWeight='bold'
+        fontSize='lg'
+        color={theme.colors.red[500]}
+      >
+        {text}
+      </Text>
+    </MotiView>
+  );
+}
+
+export default function ExitBeforeEnter() {
+  const [visible, toggle] = useReducer((s) => !s, true);
+  return (
+    <Pressable
+      onPress={toggle}
+      flex={1}
+      alignItems='center'
+      justifyContent='center'
+      flexDirection='row'
+      backgroundColor={theme.colors.secondary[100]}
+    >
+      <AnimatePresence exitBeforeEnter>
+        {visible && (
+          <AnimatedButton
+            bg={theme.colors.primary[400]}
+            text='Button'
+            visible={visible}
+          />
+        )}
+        {!visible && (
+          <AnimatedButton
+            bg={theme.colors.primary[900]}
+            text='Button'
+            visible={visible}
+          />
+        )}
+      </AnimatePresence>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  shape: {
+    justifyContent: "center",
+    height: 50,
+    width: 250,
+    borderRadius: 10,
+    backgroundColor: theme.colors.primary[900],
+  },
+});
