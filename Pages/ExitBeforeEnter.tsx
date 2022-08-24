c3e51daf1b9d349568e6a00821cc9d878530cdb9
Sankalp Nihal Pandey <sankalpnihalpandey@Sankalp-nihal-Pandey-MacBook-Air.local>
2022-08-24 18:49:54 +0530

    animation

--- Pages/ExitBeforeEnter.tsx
+++ Pages/ExitBeforeEnter.tsx
@@ -1,89 +1,214 @@
 import { AnimatePresence } from "framer-motion";
-import React, { useReducer } from "react";
-import { StyleSheet } from "react-native";
+import React, { useReducer, useState } from "react";
+
 import { MotiView } from "moti";
-import { Text, theme, Button } from "native-base";
+import {
+  Text,
+  theme,
+  Factory,
+  Stack,
+  Box,
+  VStack,
+  Button,
+  Pressable,
+  Center,
+} from "native-base";
 
 function AnimatedButton({
   bg,
-  text,
   visible,
+  transformBg,
+  toggleHandler,
 }: {
   bg: string;
-  text: string;
   visible: boolean;
+  transformBg: any;
+  toggleHandler: () => void;
 }) {
+  const FactoryView = Factory(MotiView);
+
   return (
-    <MotiView
-      from={{
-        opacity: 0,
-        scale: 0.5,
-      }}
-      animate={{
-        opacity: 1,
-        scale: 1,
-      }}
-      transition={{ type: "timing", duration: 1000, delay: 10 }}
-      exit={{
-        opacity: 0,
-        scale: 0.9,
-      }}
-      style={[
-        styles.shape,
-        {
-          backgroundColor: bg,
-        },
-      ]}
-    >
-      <Text
-        textAlign='center'
-        fontWeight='bold'
-        fontSize='lg'
-        color={theme.colors.red[500]}
+    <VStack justifyContent='center' alignItems='center' flex={1}>
+      <Pressable
+        onPress={() => {
+          toggleHandler();
+        }}
       >
-        {text}
-      </Text>
-    </MotiView>
+        <FactoryView bg='amber.400' />
+      </Pressable>
+
+      <Pressable
+        onPress={() => {
+          toggleHandler();
+        }}
+      >
+        <FactoryView
+          from={{
+            opacity: 0,
+            scale: 0.5,
+            // backgroundColor: bg,
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            // backgroundColor: transformBg,
+          }}
+          transition={{ type: "timing", duration: 500, delay: 10 }}
+          exit={{
+            opacity: 0,
+            scale: 0.9,
+          }}
+        />
+      </Pressable>
+    </VStack>
+
+    // <MotiView
+    //   from={{
+    //     opacity: 0,
+    //     scale: 0.5,
+    //   }}
+    //   animate={{
+    //     opacity: 1,
+    //     scale: 1,
+    //   }}
+    //   transition={{ type: "timing", duration: 1000, delay: 10 }}
+    //   exit={{
+    //     opacity: 0,
+    //     scale: 0.9,
+    //   }}
+    // >
+    //   <Text
+    //     textAlign='center'
+    //     fontWeight='bold'
+    //     fontSize='lg'
+    //     color={theme.colors.red[500]}
+    //   >
+    //     {text}
+    //   </Text>
+    // </MotiView>
   );
 }
 
-export default function ExitBeforeEnter() {
-  const [visible, toggle] = useReducer((s) => !s, true);
+const NBMotiView = ({
+  from,
+  to,
+  transition,
+  animate,
+  children,
+  ...props
+}: any) => {
+  const FactoryView = Factory(MotiView);
+
+  // from={{
+  //   p: 10,
+  // }}
+  // animate={{
+  //   p: 20,
+  // }}
+  // transition={{ type: "timing", duration: 1000, delay: 10 }}
+
+  const resolvedProps = {
+    from: {
+      padding: 10,
+    },
+    animate: {
+      padding: 40,
+    },
+    transition: { type: "timing", duration: 1000, delay: 10 },
+  };
+  console.log(props);
   return (
-    <Button
-      onPress={toggle}
-      flex={1}
-      alignItems='center'
-      justifyContent='center'
-      flexDirection='row'
-      backgroundColor={theme.colors.secondary[100]}
-    >
+    <FactoryView {...resolvedProps} {...props}>
+      {children}
+    </FactoryView>
+  );
+};
+export default function ExitBeforeEnter() {
+  const [visible, setVisible] = useState(true);
+  const toggleHandler = () => {
+    setVisible(!visible);
+  };
+
+  const FactoryView = Factory(MotiView, {
+    baseStyle: {
+      justifyContent: "center",
+      height: 100,
+      width: 250,
+      borderRadius: 10,
+      backgroundColor: "red.500",
+    },
+  });
+
+  return (
+    <VStack justifyContent={"center"} alignItems='center' flex={1}>
+      <Pressable
+        onPress={() => {
+          toggleHandler();
+        }}
+      >
+        <Text>Button</Text>
+      </Pressable>
+
+      {/* {visible && (
+        <MotiView
+          from={{
+            // opacity: 0,
+            // scale: 0.5,
+            backgroundColor: "red",
+            padding: 10,
+          }}
+          animate={{
+            // opacity: 1,
+            // scale: 1,
+            backgroundColor: "blue",
+            padding: 20,
+          }}
+          transition={{ type: "timing", duration: 1000, delay: 10 }}
+        >
+          <Text>hello</Text>
+        </MotiView>
+      )} */}
+
+      {visible && (
+        <NBMotiView
+          from={{
+            height: 50,
+            width: 50,
+            p: 10,
+          }}
+          animate={{
+            p: 20,
+          }}
+          transition={{ type: "timing", duration: 1000, delay: 10 }}
+          bg='blue.200'
+        >
+          <Text>hello</Text>
+        </NBMotiView>
+      )}
+
+      {/* <FactoryView bg='amber.400' /> */}
+    </VStack>
+  );
+  return (
+    <>
       <AnimatePresence exitBeforeEnter>
-        {visible && (
+        {/* {visible && (
           <AnimatedButton
-            bg={theme.colors.primary[400]}
-            text='Button'
+            bg='primary.800'
+            transformBg=''
+            toggleHandler={toggleHandler}
             visible={visible}
           />
         )}
         {!visible && (
           <AnimatedButton
-            bg={theme.colors.primary[900]}
-            text='Button'
+            bg='primary.500'
+            transformBg=''
+            toggleHandler={toggleHandler}
             visible={visible}
           />
-        )}
+        )} */}
       </AnimatePresence>
-    </Button>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  shape: {
-    justifyContent: "center",
-    height: 50,
-    width: 250,
-    borderRadius: 10,
-    backgroundColor: theme.colors.primary[900],
-  },
-});
